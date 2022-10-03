205fcbc179297d1e7f97b7c989fe97063053609f
kubikowski <kubikowski0@gmail.com>
2022-10-03 13:45:53 -0600

    replace namespace with class

--- src/test/constants/scale-testing-constants.ts
+++ src/test/constants/scale-testing-constants.ts
@@ -1,27 +1,27 @@
 import { performance } from 'perf_hooks';
 
 /**
- * Scale Testing:
+ * Scale Testing Sets:
  *
  * Each set listed below contains all the multiples of a given number
  * from `0` up to `10 Million`.
  *
  * Note: Sets with `100 Million` values will fail to instantiate.
  */
-export namespace multiples {
-	export function of1(): ReadonlySet<number> {
-		return of(1, 10_000_000);
+export abstract class Multiples {
+	public static of1(): ReadonlySet<number> {
+		return this.of(1, 10_000_000);
 	}
 
-	export function of2(): ReadonlySet<number> {
-		return of(2, 5_000_000);
+	public static of2(): ReadonlySet<number> {
+		return this.of(2, 5_000_000);
 	}
 
-	export function of3(): ReadonlySet<number> {
-		return of(3, 3_333_333);
+	public static of3(): ReadonlySet<number> {
+		return this.of(3, 3_333_333);
 	}
 
-	function of(factor: number, size: number): ReadonlySet<number> {
+	private static of(factor: number, size: number): ReadonlySet<number> {
 		return new Set<number>(Array.from(
 			{ length: size },
 			(_, index) => index * factor),
--- src/test/scale.test.ts
+++ src/test/scale.test.ts
@@ -4,12 +4,12 @@ import { equivalence } from '../functions/equivalence.function';
 import { intersection } from '../functions/intersection.function';
 import { union } from '../functions/union.function';
 import { xor } from '../functions/xor.function';
-import { multiples, time } from './constants/scale-testing-constants';
+import { Multiples, time } from './constants/scale-testing-constants';
 
 describe('Scale Tests', () => {
-	const multiplesOf1 = time('copying 10_000_000', multiples.of1);
-	const multiplesOf2 = time('copying 5_000_000', multiples.of2);
-	const multiplesOf3 = time('copying 3_333_333', multiples.of3);
+	const multiplesOf1 = time('copying 10_000_000', Multiples.of1);
+	const multiplesOf2 = time('copying 5_000_000', Multiples.of2);
+	const multiplesOf3 = time('copying 3_333_333', Multiples.of3);
 
 	it('difference scale tests', () => {
 		const result1 = time('difference of 1', () => difference(multiplesOf1));
