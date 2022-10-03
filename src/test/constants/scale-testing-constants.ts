56fa0651ea0bb4fc6039a3d0ace8b63a55ecfebd
kubikowski <44616042+kubikowski@users.noreply.github.com>
2022-10-03 13:49:58 -0600

    added scale tests (#9)
    * added scale tests
    
    * refactored beforeAll into consts
    
    * fixed import style
    
    * refactored multiples.of
    
    * replace namespace with class

---
+++ src/test/constants/scale-testing-constants.ts
@@ -0,0 +1,39 @@
+import { performance } from 'perf_hooks';
+
+/**
+ * Scale Testing Sets:
+ *
+ * Each set listed below contains all the multiples of a given number
+ * from `0` up to `10 Million`.
+ *
+ * Note: Sets with `100 Million` values will fail to instantiate.
+ */
+export abstract class Multiples {
+	public static of1(): ReadonlySet<number> {
+		return this.of(1, 10_000_000);
+	}
+
+	public static of2(): ReadonlySet<number> {
+		return this.of(2, 5_000_000);
+	}
+
+	public static of3(): ReadonlySet<number> {
+		return this.of(3, 3_333_333);
+	}
+
+	private static of(factor: number, size: number): ReadonlySet<number> {
+		return new Set<number>(Array.from(
+			{ length: size },
+			(_, index) => index * factor),
+		);
+	}
+}
+
+export function time<T>(methodName: string, method: () => T): T {
+	const timeStart = performance.now();
+	const result: T = method();
+	const timeEnd = performance.now();
+
+	console.log(`${ methodName } took ${ (timeEnd - timeStart).toFixed(3) } ms`);
+	return result;
+}
---
+++ src/test/scale.test.ts
@@ -0,0 +1,63 @@
+import { expect, it } from '@jest/globals';
+import { difference } from '../functions/difference.function';
+import { equivalence } from '../functions/equivalence.function';
+import { intersection } from '../functions/intersection.function';
+import { union } from '../functions/union.function';
+import { xor } from '../functions/xor.function';
+import { Multiples, time } from './constants/scale-testing-constants';
+
+describe('Scale Tests', () => {
+	const multiplesOf1 = time('copying 10_000_000', Multiples.of1);
+	const multiplesOf2 = time('copying 5_000_000', Multiples.of2);
+	const multiplesOf3 = time('copying 3_333_333', Multiples.of3);
+
+	it('difference scale tests', () => {
+		const result1 = time('difference of 1', () => difference(multiplesOf1));
+		const result2 = time('difference of 2', () => difference(multiplesOf1, multiplesOf2));
+		const result3 = time('difference of 3', () => difference(multiplesOf1, multiplesOf2, multiplesOf3));
+
+		expect(result1.size).toBe(10_000_000);
+		expect(result2.size).toBe(5_000_000);
+		expect(result3.size).toBe(3_333_334);
+	});
+
+	it('equivalence scale tests', () => {
+		const result1 = time('equivalence of 1', () => equivalence(multiplesOf1));
+		const result2 = time('equivalence of 2', () => equivalence(multiplesOf1, multiplesOf2));
+		const result3 = time('equivalence of 3', () => equivalence(multiplesOf1, multiplesOf2, multiplesOf3));
+
+		expect(result1).toBe(true);
+		expect(result2).toBe(false);
+		expect(result3).toBe(false);
+	});
+
+	it('intersection scale tests', () => {
+		const result1 = time('intersection of 1', () => intersection(multiplesOf1));
+		const result2 = time('intersection of 2', () => intersection(multiplesOf1, multiplesOf2));
+		const result3 = time('intersection of 3', () => intersection(multiplesOf1, multiplesOf2, multiplesOf3));
+
+		expect(result1.size).toBe(10_000_000);
+		expect(result2.size).toBe(5_000_000);
+		expect(result3.size).toBe(1_666_667);
+	});
+
+	it('union scale tests', () => {
+		const result1 = time('union of 1', () => union(multiplesOf1));
+		const result2 = time('union of 2', () => union(multiplesOf1, multiplesOf2));
+		const result3 = time('union of 3', () => union(multiplesOf1, multiplesOf2, multiplesOf3));
+
+		expect(result1.size).toBe(10_000_000);
+		expect(result2.size).toBe(10_000_000);
+		expect(result3.size).toBe(10_000_000);
+	});
+
+	it('xor scale tests', () => {
+		const result1 = time('xor of 1', () => xor(multiplesOf1));
+		const result2 = time('xor of 2', () => xor(multiplesOf1, multiplesOf2));
+		const result3 = time('xor of 3', () => xor(multiplesOf1, multiplesOf2, multiplesOf3));
+
+		expect(result1.size).toBe(10_000_000);
+		expect(result2.size).toBe(5_000_000);
+		expect(result3.size).toBe(3_333_334);
+	});
+});
