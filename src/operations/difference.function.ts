c532df4a57b7fd28c7b57351b4fefe307d0d69bc
kubikowski <44616042+kubikowski@users.noreply.github.com>
2022-10-11 14:46:33 -0600

    marginally improved operations performances (#21)
    * marginally improved operations performances
    
    * improved scale testing platform

--- src/operations/difference.function.ts
+++ src/operations/difference.function.ts
@@ -14,9 +14,9 @@ export function difference<T, S extends ReadonlySet<T>>(...sets: S[]): S {
 	const result = new Set<T>(sets[0] ?? new Set<T>());
 
 	for (let index = 1; index < sets.length; index++) {
-		sets[index]?.forEach(value => {
+		for (const value of sets[index]!) {
 			result.delete(value);
-		});
+		}
 	}
 
 	return result as ReadonlySet<T> as S;
