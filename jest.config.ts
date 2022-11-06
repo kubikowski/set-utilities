b691b7100c8b1f12e9a5e1dab32e6cb51c90abae
kubikowski <kubikowski0@gmail.com>
2022-11-06 14:30:59 -0700

    only collect coverage from src

--- jest.config.ts
+++ jest.config.ts
@@ -22,7 +22,9 @@ export default {
 	// collectCoverage: false,
 
 	// An array of glob patterns indicating a set of files for which coverage information should be collected
-	// collectCoverageFrom: undefined,
+	collectCoverageFrom: [
+		'src/**/*',
+	],
 
 	// The directory where Jest should output its coverage files
 	// coverageDirectory: undefined,
