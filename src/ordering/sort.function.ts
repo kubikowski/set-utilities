4360b11aafe869f86644cd32e60e189573a84a18
kubikowski <kubikowski0@gmail.com>
2022-11-29 12:08:10 -0700

    improved sort documentation

--- src/ordering/sort.function.ts
+++ src/ordering/sort.function.ts
@@ -2,11 +2,15 @@ export function sort<T>(set: Set<T>, compareFunction?: (a: T, b: T) => number): 
 export function sort<T>(set: ReadonlySet<T>, compareFunction?: (a: T, b: T) => number): ReadonlySet<T>;
 
 /**
- * Unlike JavaScript's native Array sort, this implementation
- * of Set sort does not modify the original Set.
+ * Unlike JavaScript's native array sort, this set sort
+ * does not modify the ordering of the original set.
  *
- * Instead, it returns a new sorted Set
- * with the same elements contained in the original.
+ * Instead, it returns a new sorted set with
+ * the same elements contained in the original.
+ *
+ * Sort is often notated A ⇅,
+ * or specifically as A ↑ or A ↓ depending on whether
+ * the sort order is ascending or descending respectively.
  */
 export function sort<T, S extends ReadonlySet<T>>(set: S, compareFunction?: (a: T, b: T) => number): S {
 	return new Set<T>(Array.from(set).sort(compareFunction)) as ReadonlySet<T> as S;
